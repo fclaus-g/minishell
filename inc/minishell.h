7a8232405c138f86122b10d704fcf92faa6af8cf
Pedro jesus Gomez ruiz <pgomez-r@c3r1s6.42malaga.com>
2023-11-14 10:03:54 +0100

    cascarabash

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 22:04:00 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/11/13 11:45:05 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/11/14 09:56:57 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include "./libft/libft.h"
 
 /*STRUCTS*/
-
+/*Estructura para cada variable de entorno (tendremos que crear un array de
+este tipo de struct)*/
 typedef struct s_env
 {
 	char	*full;
@@ -24,24 +25,41 @@ typedef struct s_env
 	char	*line;
 }	t_env;
 
+/*Estructura "general", iremos añadiendo elementos según vayamos necesitando*/
 typedef struct s_data
 {
 	char	**env_dup;
 	t_env	*env_arr;
 }	t_data;
-/*De momento no se me ocurre nada para la struct general, conforme vayamos
-probando cosas nos daremos cuenta de qué necesitamos meter*/
 
+/*Estructura para cada elemento del input, guarda tipo y su contenido en str*/
+typedef struct s_element
+{
+	char	type;
+	char	*data;
+}	t_element;
+
+/*Estructura para la lectura del input completo, dentro guardamos varias flags
+o info general y un array con todos los elementos de la linea, esto lo re-
+utilizamos en cada lectura*/
 typedef struct s_input
 {
-	char	*str_in;
-	int		is_bltn;
-	int		is_file;
-	int		n_pips;
-	char	type;
-	char	*name;
+	int			n_elements; //número de elementos
+	int			is_built; //hay built-in?
+	int			is_redir; //hay redirecciones?
+	int			n_pipe; //cuantos pipes?
+	char		**sp_input; //splitted input en matriz de cadenas
+	t_element	*elements; //array de structs - cada una un elemento
 }	t_input;
 
 /*main.c*/
+
+/*init.c*/
+void					ft_fill_input(t_input *in, char *st);
+void					ft_split_env(t_data *d, char *var, size_t x);
+void					ft_init(t_data *d, char **env);
+
+/*exit.c*/
+void					ft_clean_input(t_input *input);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,54 +6,16 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 21:52:51 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/11/13 12:56:51 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/11/14 10:02:49 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../incl/minishell.h"
-
-void	ft_split_env(t_data *d, char *var, size_t x)
-{
-	int	i;
-	int	j;
-
-	i = 1;
-	while (var[i] != '=')
-		i++;
-	d->env_arr[x].title = malloc(sizeof(char) * i);
-	i = -1;
-	while (var[++i] != '=')
-		d->env_arr[x].title[i] = var[i];
-	d->env_arr[x].title[i] = '\0';
-	i++;
-	j = i;
-	while (var[i] != '\0')
-		i++;
-	d->env_arr[x].line = malloc(sizeof(char) * (i + 1) - j);
-	i = j - 1;
-	j = -1;
-	while (var[++i] != '\0')
-		d->env_arr[x].line[++j] = var[i];
-	d->env_arr[x].line[j + 1] = '\0';
-}
-
-void	ft_init(t_data *d, char **env)
-{
-	size_t	i;
-
-	d->env_dup = env;
-	d->env_arr = malloc(sizeof(t_env) * ft_strdlen(d->env_dup));
-	i = -1;
-	while (++i < ft_strdlen(d->env_dup))
-	{
-		d->env_arr[i].full = ft_strdup(d->env_dup[i]);
-		ft_split_env(d, env[i], i);
-	}
-}
+#include "../inc/minishell.h"
 
 int	main(int ac, char **av, char **env)
 {
 	t_data	d;
+	t_input	input;
 	char	*str_input;
 	int		i = 0;
 
@@ -68,15 +30,10 @@ int	main(int ac, char **av, char **env)
 	}
 	while (1)
 	{
-		// if (elements)
-		// 	free_input;
-		str_input = readline("hackershell/> ");
+		ft_clean_input(&input);
+		str_input = readline("cascarabash/> ");
 		add_history(str_input);
-		//ft_divide_input;
-		// if (built == 0)
-		// 	ft_executor;
-		// else
-		// 	ft_built_select;
+		ft_fill_input(&input, str_input);
 	}
 	return (0);
 }
